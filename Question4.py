34ec147877700afb883ad58f3f1706669969e822
Stealeristaken <arda-orcun@hotmail.com>
2024-07-13 23:09:46 +0300

    Q4 question comment line added

--- Question4.py
+++ Question4.py
@@ -1,3 +1,11 @@
+"""
+Singular Value Decomposition (SVD) (hard)
+Write a Python function that approximates the Singular Value Decomposition on a 2x2 matrix by using 
+the jacobian method and without using numpy svd function,
+i mean you could but you wouldn't learn anything. return the result in this format.
+"""
+
+
 import numpy as np
 
 def svd_2x2_singular_values(A: np.ndarray) -> tuple:
