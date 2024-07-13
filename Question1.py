e88bac35ac07770eb3bf5cb99c39b825b47b1abd
Stealeristaken <arda-orcun@hotmail.com>
2024-07-13 23:06:09 +0300

    Q1 question comment added

--- Question1.py
+++ Question1.py
@@ -1,3 +1,10 @@
+"""
+Matrix times Vector (easy)
+Write a Python function that takes the dot product of a matrix and a vector.
+Return -1 if the matrix could not be dotted with the vector
+"""
+
+
 def matrix_dot_vector(a: list[list[int | float]], b: list[int | float]) -> list[int | float]:
     if len(a[0]) != len(b):
         return [-1]  # Return a list with -1 instead of just -1
