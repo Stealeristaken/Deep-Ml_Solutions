b901d42024b6ea4bafe0ccec75a279c49aa34ec4
Stealeristaken <arda-orcun@hotmail.com>
2024-07-13 23:07:17 +0300

    Q2 question comment added

--- Question2.py
+++ Question2.py
@@ -1,3 +1,8 @@
+"""
+Calculate Covariance Matrix (medium)
+Write a Python function that calculates the covariance matrix from a list of vectors.
+Assume that the input list represents a dataset where each vector is a feature, and vectors are of equal length.
+"""
 import numpy as np
 
 def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
