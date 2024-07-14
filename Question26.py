a292744b54ba27e2a5b86f27196522ae82a606b4
Stealeristaken <arda-orcun@hotmail.com>
2024-07-14 12:30:04 +0300

    Q26 added

---
+++ Question26.py
@@ -0,0 +1,22 @@
+"""
+Matrix times Matrix (medium)
+multiply two matrices together (return -1 if shapes of matrix dont aline), i.e. C = A dot product B
+"""
+
+def matrixmul(a:list[list[int|float]],
+              b:list[list[int|float]])-> list[list[int|float]]:
+    if len(a[0]) != len(b):
+        return -1
+    
+    vals = []
+    for i in range(len(a)):
+        hold = []
+        for j in range(len(b[0])):
+            val = 0
+            for k in range(len(b)):
+                val += a[i][k] * b[k][j]
+                           
+            hold.append(val)
+        vals.append(hold)
+
+    return vals
